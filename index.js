eb6381dc6f373f47f54468d55df55f6f9aac33be
Abhishek Rana <frontendabhi33@gmail.com>
2025-05-24 11:52:50 +0530

    final testing

--- index.js
+++ index.js
@@ -58,3 +58,83 @@ gsap.to("#img_section2", {
         }
     }
 })
+
+// Furniture Section Styling
+
+const gridWrapper = gsap.utils.toArray(".grid-items");
+
+gridWrapper.forEach(wrapper => {
+   
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach(box => {
+        gsap.from(box, {
+            y:500,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: box,
+                start: "top bottom",
+                end: "bottom top",
+                scrub: 4
+            }
+        })
+    })
+})
+
+// Change Text
+
+const heading = document.querySelector(".furniture-title h2");
+const sections = gsap.utils.toArray(".grid-wrapper");
+
+let text_h2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#furniture-section > .container",
+        start: "top 50%",
+        end: "top 50%",
+        scrub: 2,
+        ease: "ease",
+          onEnter : () => {
+            updateHeading(0)
+            gsap.set(heading, {position: "fixed", bottom: 0, zIndex: -1000})
+        },
+        onEnterBack : () => {
+            updateHeading(0);
+            gsap.set(heading, {position: "relative", bottom: 0,})
+
+        }
+
+    }
+})
+
+
+sections.forEach((section,index) => {
+    ScrollTrigger.create({
+        trigger: section,
+        start: "bottom-=20% bottom",
+        end: "bottom top",
+        onEnter : () => {
+            updateHeading(index);
+            if(index === 0) {
+                document.body.classList.remove("dark-theme")
+            }
+            
+        },
+        onEnterBack : () => {
+            updateHeading(index);
+            if(index === 0) {
+                document.body.classList.add("dark-theme")
+            }
+
+        }
+    })
+})
+
+// Update Heading
+
+function updateHeading(index) {
+    const headingTexts = ["Furniture", "Decor", "Office", "Tech"];
+    heading.textContent = headingTexts[index];
+   
+}
+
+updateHeading(0)
